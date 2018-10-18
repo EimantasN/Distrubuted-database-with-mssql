a7ace1d2c62ceb00ebadafa033f4cff50eac06d6
Eimantas Noreika <noreika.eimantas@gmail.com>
2018-10-18 12:09:10 +0300

    Created new project to connect database.

---
+++ ImageGalleryAndBlog.Services/ProductService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services
+{
+    public class ProductService : IProduct
+    {
+        //Prisijungimas prie SQL database(nzn visko gerai, bet cia conenction yra užmesgamas)
+        private readonly DbContext _ctx;
+
+        public ProductService(ProductDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+    }
+}
